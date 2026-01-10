d8629855b983756bd2baf66cd7d7a18d8482abf0
DocJade <ytdocjade@gmail.com>
2026-01-10 13:07:58 -0700

    This doesn't quite work, but i'll fix it later

--- src/http/three_ds_reqwest.rs
+++ src/http/three_ds_reqwest.rs
@@ -2,8 +2,12 @@
 
 // re-implementations of reqwest
 
+use std::collections::HashMap;
 use std::fmt;
+use std::str::FromStr;
+use std::sync::Arc;
 use std::{borrow::Cow, future::Future};
+use minreq;
 
 pub use http::header::HeaderName as HeaderName;
 pub use http::header::HeaderValue as HeaderValue;
@@ -11,6 +15,8 @@ pub use url::Url as Url;
 pub use http::HeaderMap as HeaderMap;
 pub use http::Method as Method;
 pub use http::header::InvalidHeaderValue as InvalidHeaderValue;
+use http_body_util::{BodyExt, Full};
+use bytes::Bytes;
 
 pub type ReqwestResult<T> = std::result::Result<T, Error>;
 
@@ -29,19 +35,14 @@ pub trait IntoUrlSealed {
 }
 // https://github.com/seanmonstar/reqwest/blob/04a216fc17d75b4ebe4b0829ae7bbd8279c0dcab/src/into_url.rs#L40-L45
 impl IntoUrlSealed for Url {
-    fn into_url(self) -> crate::Result<Url> { unimplemented!("3DS STUB: IntoUrlSealed URL into_url") }
-    fn as_str(&self) -> &str {
-        self.as_ref()
-    }
+    fn into_url(self) -> crate::Result<Url> { Ok(self) }
+    fn as_str(&self) -> &str { self.as_ref() }
 }
 impl<'a> IntoUrlSealed for &'a str {
     fn into_url(self) -> crate::Result<Url> {
-        unimplemented!("3DS STUB: IntoUrlSealed &'a str into_url")
+        Url::parse(self).map_err(|_| crate::Error::Other("Invalid URL"))
     }
-
-    fn as_str(&self) -> &str {
-        self
-    }
+    fn as_str(&self) -> &str { self }
 }
 
 impl<'a> IntoUrlSealed for &'a String {
@@ -104,21 +105,86 @@ impl std::error::Error for Error {
 
 
 #[derive(Debug, Clone)]
-pub struct Client;
+pub struct Client {
+    config: Arc<Config>,
+}
 impl Client {
-    pub fn new() -> Self { Self }
+    pub fn new() -> Self {
+        // defaults i guess
+       
+        Self {
+            config: ClientBuilder::new().config.into()
+        }
+    }
     // https://docs.rs/reqwest/latest/reqwest/struct.Client.html#method.request
     pub fn request<U: IntoUrl>(&self, method: Method, url: U) -> RequestBuilder {
         let req = url.into_url().map(move |url| Request::new(method, url));
         RequestBuilder::new(self.clone(), req)
     }
     // https://docs.rs/reqwest/latest/reqwest/struct.Client.html#method.execute
-    pub fn execute(
-        &self,
-        request: Request,
-    ) -> impl Future<Output = Result<Response, crate::Error>> {
-        async {
-            unimplemented!("3DS STUB: CLIENT EXECUTE")
+    // https://docs.rs/reqwest/latest/reqwest/struct.Client.html#method.execute
+    pub fn execute(&self, request: Request) -> impl Future<Output = Result<Response, crate::Error>> {
+        let config = self.config.clone();
+        let url_copy = request.url.clone(); // Capture URL for printing
+        let url_string = request.url.to_string(); // Capture URL for printing
+        async move {
+            println!("HTTP: {} {}", request.method, url_string); // TODO: DEBUGGING
+            
+            // We use spawn_blocking because minreq is a blocking library.
+            // On the 3DS, running this directly in the async executor often starves the 
+            // background threads responsible for the SOC service.
+            let res = tokio::task::spawn_blocking(move || {
+                let mut min_req = minreq::Request::new(convert_http_method(request.method.clone()), request.url.as_str());
+                // timeouts!
+                min_req = min_req.with_timeout(config.timeout.unwrap_or(120));
+                
+                for (name, value) in &request.headers {
+                    min_req = min_req.with_header(name.as_str(), value.to_str().unwrap_or(""));
+                }
+
+                if let Some(body) = request.body {
+                    // You'll need to store the bytes in your Body struct
+                    min_req = min_req.with_body(body.bytes);
+                }
+                println!("Sending request...");
+                // keep track if we're stuck.
+                let mut attempts: u16 = 0;
+                // spawn with blocking to make sure we run on another thread.
+                loop {
+                    match min_req.clone().send() {
+                        Ok(res) => break Ok(res),
+                        Err(minreq::Error::IoError(ref e)) => {
+                            // yeild to let the network stack do its biz
+                            attempts += 1;
+                            
+                            // sleep a bit to hopefully un-block
+                            std::thread::sleep(std::time::Duration::from_millis(10));
+                            
+                            if attempts % 50 == 0 {
+                                println!("minreq waiting...");
+                                println!("{e:#?}")
+                            }
+                            if attempts > 1000 {
+                                return Err(crate::Error::Other("Blocked forever."));
+                            }
+                            continue;
+                        }
+                        Err(e) => {
+                            println!("Uncaught minreq error: {:?}", e);
+                            return Err(crate::Error::Other("minreq failed"));
+                        }
+                    }
+                }
+            }).await;
+
+            match res {
+                Ok(Ok(min_res)) => {
+                    println!("Response: {}", min_res.status_code);
+                    Ok(http_response_from_minreq_response(min_res, url_copy))
+                }
+                Ok(Err(e)) => Err(e),
+                Err(_) => Err(crate::Error::Other("Spawn blocking task failed")),
+            }
         }
     }
     // https://docs.rs/reqwest/latest/reqwest/struct.Client.html#method.builder
@@ -138,23 +204,39 @@ pub struct ClientBuilder {
 impl ClientBuilder {
     // https://docs.rs/reqwest/latest/reqwest/struct.ClientBuilder.html#method.new
     pub fn new() -> Self {
-        // TODO: the body is huge, fill in later
-        unimplemented!("3DS STUB: ClientBuilder NEW")
+        Self {
+            config: Config::default(),
+        }
     }
     // https://docs.rs/reqwest/latest/reqwest/struct.ClientBuilder.html#method.build
-    pub fn build(self) -> crate::Result<Client> {
-        // TODO: ditto.
-        unimplemented!("3DS STUB: ClientBuilder BUILD")
+   pub fn build(self) -> crate::Result<Client> {
+        // this is where a pool would be made, but we lazy
+        Ok(Client {
+            config: Arc::new(self.config),
+        })
     }
     // https://docs.rs/reqwest/latest/reqwest/struct.ClientBuilder.html#method.use_rustls_tls
-    pub fn use_rustls_tls(self) -> ClientBuilder {
-        // TODO: TLS stuff.
-        unimplemented!("3DS STUB: ClientBuilder RUSTLS_TLS")
+    pub fn use_rustls_tls(self) -> Self {
+        // "yeah we are using tls i promise... stop bugging me!"
+        self
     }
 }
-// todo
+#[derive(Debug)]
 struct Config {
+    user_agent: Option<HeaderValue>,
+    /// Timeout in seconds
+    timeout: Option<u64>,
+    proxy: Option<String>, 
+}
 
+impl Default for Config {
+    fn default() -> Self {
+        Self { 
+            user_agent: Some(HeaderValue::from_static("3DSCord (3DS; Nintendo 3DS)")),
+            timeout: Some(30),
+            proxy: None,
+        }
+    }
 }
 
 // https://github.com/seanmonstar/reqwest/blob/master/src/async_impl/body.rs#L373-L374C5
@@ -162,17 +244,19 @@ pub(crate) type ResponseBody =
     http_body_util::combinators::BoxBody<bytes::Bytes, Box<dyn std::error::Error + Send + Sync>>;
 #[derive(Debug)]
 pub struct HyperResponse<T> {
-    pub something_idk_yet: T,
+    pub status: StatusCode,
+    pub headers: HeaderMap,
+    pub body: T,
 }
 impl HyperResponse<ResponseBody> {
     pub fn headers(&self) -> &HeaderMap {
-        unimplemented!("3DS STUB: HyperResponse HEADERS")
+        &self.headers
     }
     pub fn status(&self) -> StatusCode {
-        unimplemented!("3DS STUB: HyperResponse STATUS")
+        self.status
     }
     fn into_body(self) -> ResponseBody { // not sure if this is the right return type.
-        unimplemented!("3DS STUB: HyperResponse INTO_BODY")
+        self.body
     }
 }
 
@@ -259,7 +343,9 @@ impl Part {
     }
 }
 
-pub struct Body;
+pub struct Body {
+    bytes: Vec<u8>
+}
 
 impl Body {
     // todo
@@ -303,25 +389,32 @@ pub struct RequestBuilder {
 }
 impl RequestBuilder {
     // https://docs.rs/reqwest/latest/reqwest/struct.RequestBuilder.html#method.header
-    pub fn header<K, V>(self, key: K, value: V) -> RequestBuilder
-    where
-        HeaderName: TryFrom<K>,
-        <HeaderName as TryFrom<K>>::Error: Into<http::Error>,
-        HeaderValue: TryFrom<V>,
-        <HeaderValue as TryFrom<V>>::Error: Into<http::Error>,
+    pub fn header<K, V>(mut self, key: K, value: V) -> Self 
+    where 
+        HeaderName: TryFrom<K>, <HeaderName as TryFrom<K>>::Error: Into<http::Error>,
+        HeaderValue: TryFrom<V>, <HeaderValue as TryFrom<V>>::Error: Into<http::Error>,
     {
-        unimplemented!("3DS STUB: REQUESTBUILDER HEADER")
+        if let Ok(ref mut req) = self.request {
+            if let (Ok(k), Ok(v)) = (HeaderName::try_from(key), HeaderValue::try_from(value)) {
+                req.headers.insert(k, v);
+            }
+        }
+        self
     }
     // make bodies out of bytes
     // https://docs.rs/reqwest/latest/reqwest/struct.RequestBuilder.html#method.body
-    pub fn body<T: Into<Body>>(self, body: T) -> Self {
-        unimplemented!("3DS STUB: REQUESTBUILDER BODY")
+    pub fn body<T: Into<Body>>(mut self, body: T) -> Self {
+        if let Ok(ref mut req) = self.request {
+            req.body = Some(body.into());
+        }
+        self
     }
     // TODO: due to how the 3ds does multithreaded this is probably sync, how do we not block?
     // https://docs.rs/reqwest/latest/reqwest/struct.RequestBuilder.html#method.send
     pub fn send(self) -> impl Future<Output = Result<Response, crate::Error>> {
-        async {
-            unimplemented!("3DS STUB: REQUESTBUILDER SEND");
+        async move {
+            let req = self.request.map_err(|_| crate::Error::Other("Failed to build request."))?;
+            self.client.execute(req).await
         }
     }
 
@@ -331,15 +424,88 @@ impl RequestBuilder {
     }
 
     // https://docs.rs/reqwest/latest/reqwest/struct.RequestBuilder.html#method.headers
-    pub fn headers(self, headers: HeaderMap) -> Self {
-        unimplemented!("3DS STUB: REQUESTBUILDER HEADERS")
+    pub fn headers(mut self, headers: HeaderMap) -> Self {
+        if let Ok(ref mut request) = self.request {
+            // extend those headers
+            request.headers.extend(headers);
+        }
+        self
     }
     
     // https://docs.rs/reqwest/latest/reqwest/struct.RequestBuilder.html#method.build
     pub fn build(self) -> crate::Result<Request> {
         self.request
     }
-    pub fn new(client: Client, request: crate::Result<Request>) -> RequestBuilder {
-        unimplemented!("3DS STUB: REQUESTBUILDER NEW")
+    pub fn new(client: Client, request: crate::Result<Request>) -> Self {
+        Self { client, request }
     }
+}
+
+
+/// Convert http::Method to minireq::Method
+fn convert_http_method(method: http::Method) -> minreq::Method {
+    match method {
+        Method::GET => {
+            minreq::Method::Get
+        }
+        Method::POST => {
+            minreq::Method::Post
+        }
+        Method::PUT => {
+            minreq::Method::Put
+        }
+        Method::DELETE => {
+            minreq::Method::Delete
+        }
+        Method::HEAD => {
+            minreq::Method::Head
+        }
+        Method::OPTIONS => {
+            minreq::Method::Options
+        }
+        Method::CONNECT => {
+            minreq::Method::Connect
+        }
+        Method::PATCH => {
+            minreq::Method::Patch
+        }
+        Method::TRACE => {
+            minreq::Method::Trace
+        }
+        _ => {
+            // No idea what these do.
+            unimplemented!("Cannot convert http method ({method:#?}) to minreq method!")
+        }
+    }
+}
+
+fn http_response_from_minreq_response(response: minreq::Response, url: impl Into<Box<Url>>) -> Response {
+    let status: StatusCode = StatusCode::from_u16(response.status_code as u16).expect("Unable to convert status code!");
+    let headers = http_headermap_from_minreq_headers(&response.headers);
+    let raw_body = response.into_bytes();
+    let bytes = Bytes::from(raw_body);
+    // "Full body never errors on collect"
+    let body: ResponseBody = Full::new(bytes).map_err(|never| match never {}).boxed();
+    Response {
+        res: HyperResponse {
+            status,
+            headers,
+            body
+        },
+        url: url.into(),
+    }
+}
+
+fn http_headermap_from_minreq_headers(headers: &HashMap<String, String>) -> http::HeaderMap {
+    let mut header_map: HeaderMap = HeaderMap::new();
+    for (name, value) in headers {
+        // get the header name
+        if let Ok(header_name) = HeaderName::from_str(name) {
+            // now get the value
+            if let Ok(header_value) = HeaderValue::from_str(value) {
+                header_map.insert(header_name, header_value);
+            }
+        }
+    }
+    header_map
 }
